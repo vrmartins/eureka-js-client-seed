ed2512de8fd0f921200a2e119b816854500826b3
Vilmar Rodrigues Martins <vilmarmartins@softbox.com.br>
2019-03-14 10:57:43 -0300

    feat(events) Added events

--- index.js
+++ index.js
@@ -6,7 +6,21 @@ app.get('/info', (req, res) => {
   res.json({message: `It's work`})
 })
 
-eurekaClient.start((error) => console.log('error', error))
-// eurekaClient.stop((error) => console.log('error', error))
+// Events
+eurekaClient.on('started', () => console.log(`started in: ${Date().toString()}`))
+eurekaClient.on('registered', () => console.log(`registered in: ${Date().toString()}`))
+eurekaClient.on('deregistered', () => console.log(`deregistered in: ${Date().toString()}`))
+eurekaClient.on('heartbeat', () => console.log(`heartbeat in: ${Date().toString()}`))
+eurekaClient.on('registryUpdated', () => console.log(`registryUpdated in: ${Date().toString()}`))
+
+// Comment to stop and uncomment to stop
+eurekaClient.start((error) => {
+  if (error) console.log('error', error)
+})
+
+// Uncomment to stop and comment to start
+// eurekaClient.stop((error) => {
+//   if (error) console.log('error', error)
+// })
 
 app.listen('3000', () => console.log(`Started server on 3000`))
